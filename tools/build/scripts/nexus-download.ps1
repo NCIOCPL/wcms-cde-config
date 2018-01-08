af29d0cdc839a2b6dcdc9661f9ea559353da8a58
Blair <blairlearn@users.noreply.github.com>
2018-01-08 17:46:34 -0500

    Fix config file load.
    Include script's running directory in order to determine location of the
    nexus-config.json file.

--- tools/build/scripts/nexus-download.ps1
+++ tools/build/scripts/nexus-download.ps1
@@ -13,6 +13,9 @@
 
 	.PARAMETER UserPass
 	The Nexus user's Password. Required.
+
+	.PARAMETER Destination
+	The location for storing the downloaded file.
 #>
 
 Param(
@@ -23,11 +26,19 @@ Param(
 	[string]$UserID,
 
 	[Parameter(mandatory=$true, ValueFromPipeline=$false)]
-	[string]$UserPass
+	[string]$UserPass,
+
+	[Parameter(mandatory=$false, ValueFromPipeline=$false)]
+	[string]$Destination = "."
 )
 
+# Calculate the save file name.
+$SaveLocation = Join-Path -path $Destination $Filename
+
+
 # Calculate the upload destination URL.
-$config = (Get-Content "nexus-config.json") -join "`n" | ConvertFrom-Json
+$configFile = [System.IO.Path]::Combine($PSScriptRoot, "nexus-config.json")
+$config = (Get-Content $configFile) -join "`n" | ConvertFrom-Json
 $remoteUrl = $config.baseUrl.Trim()
 if ( -not $remoteUrl.EndsWith('/') ) { $remoteUrl = $remoteUrl + '/' }
 $remoteUrl = $remoteUrl + $Filename
@@ -37,13 +48,13 @@ $securePassword = ConvertTo-SecureString $UserPass -AsPlainText -Force
 $credential = New-Object PSCredential ($UserID, $securePassword)
 
 
-$uploadParams = @{
+$downloadParams = @{
 	Uri = $remoteUrl;
 	Method = 'GET';
 	Credential = $credential;
-	OutFile = $Filename;
+	OutFile = $SaveLocation;
 }
 
-$result = Invoke-RestMethod @uploadParams
+$result = Invoke-RestMethod @downloadParams
 
 Write-Host $result
